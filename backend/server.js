a44f1f62d0da3bf80626eee46e518992ab40ea92
Siddharth Roy <siddharthroy36912@gmail.com>
2020-12-13 17:12:02 +0530

    fixed no such file or directory error

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use('/uploads', express.static('uploads'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
     app.get('*', (req, res) => 
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'))
     )
 } else {
     app.get('/', (req, res) => {
